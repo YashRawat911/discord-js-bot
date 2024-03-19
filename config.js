e06e4c99b37e6fb59a51b716a4e5cc9edae1a1cc
YashRawat <139982135+YashRawat911@users.noreply.github.com>
2024-03-19 07:22:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,8 +37,8 @@ module.exports = {
 
   DASHBOARD: {
     enabled: true, // enable or disable dashboard
-    baseURL: "http://localhost:8080", // base url
-    failureURL: "http://localhost:8080", // failure redirect url
+    baseURL: "https://betamod.zc.al", // base url
+    failureURL: "https://betamod.zc.al", // failure redirect url
     port: "8080", // port to run the bot on
   },
 
